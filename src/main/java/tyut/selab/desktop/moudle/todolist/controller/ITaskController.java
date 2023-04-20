6496d2c85f34edd7ac28e5f8628017db33ef7224
MrTry-Zhao <117160816+MrTry-Zhao@users.noreply.github.com>
2023-04-20 19:48:58 +0800

    12

---
+++ src/main/java/tyut/selab/desktop/moudle/todolist/controller/ITaskController.java
@@ -0,0 +1,41 @@
+package tyut.selab.desktop.moudle.todolist.controller;
+
+import tyut.selab.desktop.moudle.todolist.domain.vo.TaskVo;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ITaskController {
+    /**
+     * 到期提醒
+     */
+    void taskWarnTime(TaskVo taskVo);
+
+    /**
+     * 查询该用户的全部任务
+     * @param studentNumber
+     * @return
+     */
+    List<TaskVo> queryAllTask(Integer studentNumber) throws SQLException, NoSuchFieldException, InstantiationException, IllegalAccessException;
+
+    /**
+     * 增加用户任务
+     * @param task
+     * @return
+     */
+    int insertTask(TaskVo task) throws SQLException;
+
+    /**
+     * 更新
+     * @param task
+     * @return
+     */
+    int updateTask(TaskVo task) throws SQLException;
+
+    /**
+     * 删除
+     * @param task
+     * @return
+     */
+    int deleteTask(TaskVo task) throws SQLException;
+}
